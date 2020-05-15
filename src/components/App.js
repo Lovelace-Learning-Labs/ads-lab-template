939ecfcd988815132533b42db4c8d6d18f8f73a9
Dan Roberts <dan@adadevelopersacademy.org>
2020-05-15 11:51:01 -0700

    Make the basic webpage / app.js be more of a template

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>ADS Project Template</h1>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.js';
+
+
+ReactDOM.render(
+  React.createElement(App),
+  document.querySelector('.app-main')
+);
